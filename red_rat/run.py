98156016e66be963c31d642c324b724736567313
crlqjx <quijouxcyril@outlook.com>
2019-11-17 23:50:19 +0100

    add run file

---
+++ red_rat/run.py
@@ -0,0 +1,11 @@
+from red_rat.mongo import MongoConnector
+from red_rat.market_data_provider import MarketDataProvider
+
+
+def update_quotes():
+    tickers = ['PVL.PA', 'FR.PA']
+    return
+
+
+if __name__ == '__main__':
+    update_quotes()
