dbec225d8f47866612643512c05abea3019f5b93
crlqjx <quijouxcyril@outlook.com>
2020-07-07 20:52:52 +0200

    compute portfolio vol and sharpe ratios

--- red_rat/app/risk.py
+++ red_rat/app/risk.py
@@ -2,14 +2,48 @@ from red_rat.app.portfolio import Portfolio
 
 
 class PortfolioRiskManager(Portfolio):
-    def __init__(self, portfolio_path=None):
+    def __init__(self, risk_free_rate: float, is_volatility_from_nav: bool = True, portfolio_path: str = None):
         super().__init__(portfolio_path)
 
-    def portfolio_volatility(self, start, period):
+        self._is_volatility_from_nav = is_volatility_from_nav
+
+        self._portfolio_volatility = None
+        self._portfolio_sharpe_ratios = None
+
+        self._compute_portfolio_volatility()
+        self._compute_portfolio_sharpe_ratios(risk_free_rate)
+
+    def _compute_portfolio_volatility(self):
+        """
+        Compute portfolio volatility since inception
+        :return: float value
+        """
+        if self._is_volatility_from_nav:
+            self._portfolio_volatility = self.portfolio_weekly_returns.std()
+        else:
+            # TODO: compute portfolio vol from stocks vol and corr
+            pass
         return
 
-    def portfolio_sharpe_ratio(self, risk_free_rate):
+    def _compute_portfolio_sharpe_ratios(self, risk_free_rate):
+        """
+        compute portfolio sharpe ratios compared to a fixed risk-free rate
+        :param risk_free_rate: float
+        :return: series
+        """
+
+        sharpe_ratios = (self.portfolio_weekly_returns - risk_free_rate) / self._portfolio_volatility
+        self._portfolio_sharpe_ratios = sharpe_ratios
+
         return
 
-    def portfolio_value_at_risk(self):
+    def _compute_portfolio_value_at_risk(self):
         return
+
+    @property
+    def portfolio_volatility(self):
+        return self._portfolio_volatility
+
+    @property
+    def portfolio_sharpe_ratios(self):
+        return self._portfolio_sharpe_ratios
