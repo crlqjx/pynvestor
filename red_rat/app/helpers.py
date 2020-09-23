8b735944f74dbcdbee42394ffca1aea31ebe0cbc
crlqjx <quijouxcyril@outlook.com>
2020-09-23 19:53:09 +0200

    error message more detailed

--- red_rat/app/helpers.py
+++ red_rat/app/helpers.py
@@ -26,7 +26,7 @@ class Helpers:
         try:
             quotes_series = pd.DataFrame(quotes).set_index('time')['price']
         except KeyError as key_error:
-            raise KeyError(f'could not find quotes for {isin} - {key_error}')
+            raise KeyError(f'could not find quotes for {isin} between {start_date} and {end_date}- {key_error}')
         quotes_series.index = quotes_series.index.normalize()
         return quotes_series
 
