87fd9409a02cd7302e5e9855fb4935df3c801c04
crlqjx <quijouxcyril@outlook.com>
2020-09-30 19:17:32 +0200

    implementing async requests

--- red_rat/app/data_providers.py
+++ red_rat/app/data_providers.py
@@ -10,6 +10,7 @@ from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.util.retry import Retry
 from datetime import date
 from red_rat import logger
+from typing import List, Tuple
 
 current_directory = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 with open(os.path.join(current_directory, 'config.json'), 'r') as config_file:
@@ -107,6 +108,53 @@ class EuronextClient(MarketDataProvider):
             logger.log.warning(f'No quotes for {isin}-{mic}')
         return result
 
+    @logger
+    def get_quotes_multiple_stocks(self, isin_mic_period: List[Tuple]) -> List[List[dict]]:
+        """
+        Get quotes of stocks asynchronously
+        :param isin_mic_period: list of tuples with the following format isin, mic and period (max or intraday)
+        [('FR123456789', 'XPAR', 'max'), ('FR987654321', 'XPAR', 'intraday')]
+        :return:
+        """
+        async def fetch(isin, mic, period):
+            url = f"{self._base_url}/intraday_chart/getChartData/{isin}-{mic}/{period}"
+            try:
+                assert period in ['max', 'intraday'], f'{isin}: period {period} is not available'
+                async with aiohttp.ClientSession() as session:
+                    async with session.get(url) as response:
+                        response_json = response.json()
+                        quotes = await response_json
+                        for quote in quotes:
+                            quote['time'] = dt.datetime.strptime(quote['time'], "%Y-%m-%d %H:%M")
+                            quote.update({'isin': isin, 'mic': mic})
+                        return quotes
+            except aiohttp.ClientOSError as client_os_error:
+                logger.log.warning(f'{client_os_error} - Retrying...')
+            except AssertionError as assertion_error:
+                logger.log.warning(assertion_error)
+
+        async def fetch_all(stocks_to_request):
+            final_result = []
+            while stocks_to_request:
+                response_jsons = await asyncio.gather(*[fetch(isin, mic, period)
+                                                        for isin, mic, period in stocks_to_request])
+                temp_result = []
+                missing_response = []
+                for idx, quote in enumerate(response_jsons):
+                    if quote is not None:
+                        temp_result.append(quote)
+                    else:
+                        missing_response.append(idx)
+                final_result += temp_result
+                stocks_to_request = [stocks_to_request[idx] for idx in missing_response]
+                if missing_response:
+                    logger.log.info(f'Retrying for {len(stocks_to_request)} stocks')
+            return final_result
+
+        all_quotes = asyncio.run(fetch_all(isin_mic_period))
+
+        return all_quotes
+
     def get_historical_data(self, isin, market, start_date, end_date):
         # TODO
         url = 'https://live.euronext.com/fr/ajax/getHistoricalPricePopup/FR0000045072-XPAR'
@@ -114,7 +162,9 @@ class EuronextClient(MarketDataProvider):
     def update_stocks_list(self):
         today = date.today().isoformat()
         filename = os.path.join(current_directory, "static", f"Euronext_Equities_{today}.json")
-        url = 'https://live.euronext.com/fr/pd/data/stocks?mics=ALXB%2CALXL%2CALXP%2CXPAR%2CXAMS%2CXBRU%2CXLIS%2CXMLI%2CMLXB%2CENXB%2CENXL%2CTNLA%2CTNLB%2CXLDN%2CXESM%2CXMSM%2CXATL%2CVPXB&display_datapoints=dp_stocks&display_filters=df_stocks'
+        url = 'https://live.euronext.com/fr/pd/data/stocks?mics=ALXB%2CALXL%2CALXP%2CXPAR%2CXAMS%2CXBRU%2CXLIS%' \
+              '2CXMLI%2CMLXB%2CENXB%2CENXL%2CTNLA%2CTNLB%2CXLDN%2CXESM%2CXMSM%2CXATL%2CVPXB&display_datapoints=' \
+              'dp_stocks&display_filters=df_stocks'
         resp = self._session.post(url, data={'iDisplayLength': 3000})
         stock_list = resp.json()
         with open(filename, 'w') as f:
@@ -125,7 +175,8 @@ class EuronextClient(MarketDataProvider):
     def update_indices_list(self):
         today = date.today().isoformat()
         filename = os.path.join(current_directory, "static", f"Euronext_Indices_{today}.json")
-        url = 'https://live.euronext.com/pd/data/index?mics=XAMS%2CXBRU%2CXLIS%2CXPAR%2CXLDN%2CXDUB&display_datapoints=dp_index&display_filters=df_index'
+        url = 'https://live.euronext.com/pd/data/index?mics=XAMS%2CXBRU%2CXLIS%2CXPAR%2CXLDN%2CXDUB&' \
+              'display_datapoints=dp_index&display_filters=df_index'
         resp = self._session.post(url, data={'iDisplayLength': 500})
         indices_list = resp.json()
         with open(filename, 'w') as f:
@@ -157,28 +208,44 @@ class ReutersClient(MarketDataProvider):
 
     @logger
     def get_companies_profile(self, rics):
-        async def fetch(ric):
-            url = rf"https://www.reuters.com/companies/api/getFetchCompanyProfile/{ric}"
-            print(f'getting {ric} info')
+        async def fetch(reuters_code):
+            url = rf"https://www.reuters.com/companies/api/getFetchCompanyProfile/{reuters_code}"
             async with aiohttp.ClientSession() as session:
                 async with session.get(url) as response:
-                    jso = response.json()
+                    response_json = response.json(content_type=None)
                     response_status = response.status
-                    return response_status, ric, await jso
+                    return response_status, reuters_code, await response_json
 
-        async def fetch_all(rics):
-            jsons = await asyncio.gather(*[fetch(ric) for ric in rics])
-            return jsons
+        async def fetch_all(stocks_to_request):
+            retry_counter = 0
+            final_result = []
+            while stocks_to_request:
+                response_jsons = await asyncio.gather(*[fetch(stock) for stock in stocks_to_request])
+                temp_result = []
+                missing_response = []
+
+                for idx, (status, ric, profile) in enumerate(response_jsons):
+                    if status >= 404:
+                        print(status, profile)
+                        missing_response.append(idx)
+                    else:
+                        print(status)
+                        temp_result.append(profile)
+
+                final_result += temp_result
+                if stocks_to_request == [stocks_to_request[idx] for idx in missing_response]:
+                    retry_counter += 1
+                if retry_counter >= 20:
+                    logger.log.warning('Retried too many times for the same stocks, aborting')
+                    break
+
+                stocks_to_request = [stocks_to_request[idx] for idx in missing_response]
+
+                if missing_response:
+                    logger.log.info(f'Retrying for {len(stocks_to_request)} stocks')
+
+            return final_result
 
         company_profiles = asyncio.run(fetch_all(rics))
 
-        result = []
-        in_error = []
-
-        for profile in company_profiles:
-            if profile[0] >= 404:
-                in_error.append(profile)
-            else:
-                result.append(profile)
-
-        return result, in_error
+        return company_profiles
