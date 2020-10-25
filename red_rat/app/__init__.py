9ff90c5fdea3a1a4c013a8fb5f0e01a05b7fecca
crlqjx <quijouxcyril@outlook.com>
2020-10-25 22:48:23 +0100

    add YahooClient

--- red_rat/app/__init__.py
+++ red_rat/app/__init__.py
@@ -1,6 +1,7 @@
-from red_rat.app.data_providers import EuronextClient, ReutersClient
+from red_rat.app.data_providers import EuronextClient, ReutersClient, YahooClient
 from red_rat.app.mongo_connector import MongoConnector
 
 euronext = EuronextClient()
 reuters = ReutersClient()
+yahoo = YahooClient()
 mongo = MongoConnector()
