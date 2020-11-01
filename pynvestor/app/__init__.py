f4063dbccbe3f7d7078568c94b7121226063fb58
crlqjx <quijouxcyril@outlook.com>
2020-11-01 15:57:39 +0100

    move folder

--- pynvestor/app/__init__.py
+++
@@ -1,7 +0,0 @@
-from pynvestor.app.data_providers import EuronextClient, ReutersClient, YahooClient
-from pynvestor.app.mongo_connector import MongoConnector
-
-euronext = EuronextClient()
-reuters = ReutersClient()
-yahoo = YahooClient()
-mongo = MongoConnector()
